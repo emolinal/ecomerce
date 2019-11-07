4299b05e6e994c387d8031748fc5d3addd8cba14
emolinal <44538694+emolinal@users.noreply.github.com>
2019-11-07 14:32:17 -0600

    Update db.php
    update server, user, db and pass

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "tienda_tusolutionweb";
+$servername = "telecomunicacionespalencia-mysqldbserver.mysql.database.azure.com";
+$username = "mysqldbuser@telecomunicacionespalencia-mysqldbserver";
+$password = "Palencia1";
+$db = "ecomercepalencia";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
